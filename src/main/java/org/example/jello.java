3d0f0f2dd998502ed09b7b44942730875b19bd76
shownkwok <shownkwok@gmail.com>
2022-11-23 15:13:29 +0800

    67522

--- src/main/java/org/example/jello.java
+++ src/main/java/org/example/jello.java
@@ -2,8 +2,6 @@ package org.example;
 
 public class jello {
     public static void main(String[] args) {
-        System.out.println("testee");
-        System.out.println("244");
-
+        System.out.println("fe World");
     }
 }
